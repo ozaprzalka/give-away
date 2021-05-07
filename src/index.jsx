915f399c9052f3914206ebe7268444a33dfa57cb
olaz <o.zaprzalka@gmail.com>
2021-05-07 22:52:01 +0200

    wip3

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { Grommet } from "grommet";
-import { customTheme } from "./styles";
 
 ReactDOM.render(
-  <Grommet full theme={customTheme} style={{ height: "100%" }}>
-    <App />
-  </Grommet>,
+    <App />,
   document.getElementById("root")
 );
